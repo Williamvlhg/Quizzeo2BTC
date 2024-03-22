c813281836dd622b2510f12be1cff60fef067f2e
Williamvlhg <136070530+Williamvlhg@users.noreply.github.com>
2024-03-22 17:36:50 +0100

    Update quizzeo2.sql

--- quizzeo2.sql
+++ quizzeo2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : mer. 20 mars 2024 à 10:02
--- Version du serveur : 8.2.0
--- Version de PHP : 8.2.13
+-- Host: localhost:8889
+-- Generation Time: Mar 22, 2024 at 04:36 PM
+-- Server version: 5.7.39
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -15,200 +15,262 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `quizzeo2`
+-- Database: `quizzeo2`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `feedback`
+-- Table structure for table `feedback`
 --
 
-DROP TABLE IF EXISTS `feedback`;
-CREATE TABLE IF NOT EXISTS `feedback` (
-  `id_feed` int NOT NULL AUTO_INCREMENT,
-  `value_num` tinyint DEFAULT NULL,
-  `value_text` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `id_question` int NOT NULL,
-  `id_user_feed` int NOT NULL,
-  PRIMARY KEY (`id_feed`),
-  KEY `question_f` (`id_question`),
-  KEY `id_user_feed` (`id_user_feed`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+CREATE TABLE `feedback` (
+  `id_feed` int(11) NOT NULL,
+  `value_num` tinyint(4) DEFAULT NULL,
+  `value_text` text,
+  `id_question` int(11) NOT NULL,
+  `id_user_feed` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `note`
+-- Table structure for table `note`
 --
 
-DROP TABLE IF EXISTS `note`;
-CREATE TABLE IF NOT EXISTS `note` (
-  `id_note` int NOT NULL AUTO_INCREMENT,
-  `id_quizz` int NOT NULL,
-  `id_user` int NOT NULL,
-  `score` int NOT NULL,
-  PRIMARY KEY (`id_note`),
-  KEY `note_user` (`id_user`),
-  KEY `note_quizz` (`id_quizz`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+CREATE TABLE `note` (
+  `id_note` int(11) NOT NULL,
+  `id_quizz` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `score` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `qcm`
+-- Table structure for table `qcm`
 --
 
-DROP TABLE IF EXISTS `qcm`;
-CREATE TABLE IF NOT EXISTS `qcm` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `label` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+CREATE TABLE `qcm` (
+  `id` int(11) NOT NULL,
+  `label` varchar(50) NOT NULL,
   `good` tinyint(1) NOT NULL,
-  `id_question` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `question` (`id_question`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Déchargement des données de la table `qcm`
---
-
-INSERT INTO `qcm` (`id`, `label`, `good`, `id_question`) VALUES
-(1, 'FRDFDFBREB', 0, 1);
+  `id_question` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `question`
+-- Table structure for table `question`
 --
 
-DROP TABLE IF EXISTS `question`;
-CREATE TABLE IF NOT EXISTS `question` (
-  `id_question` int NOT NULL AUTO_INCREMENT,
-  `label` varchar(150) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+CREATE TABLE `question` (
+  `id_question` int(11) NOT NULL,
+  `label` varchar(150) NOT NULL,
   `date_question` date NOT NULL,
-  `type_question` int NOT NULL,
-  `id_quizz` int NOT NULL,
-  PRIMARY KEY (`id_question`),
-  KEY `quizz` (`id_quizz`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Déchargement des données de la table `question`
---
-
-INSERT INTO `question` (`id_question`, `label`, `date_question`, `type_question`, `id_quizz`) VALUES
-(1, 'RVRBRBRB', '0000-00-00', 2, 1);
+  `type_question` int(11) NOT NULL,
+  `id_quizz` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `quizz`
+-- Table structure for table `quizz`
 --
 
-DROP TABLE IF EXISTS `quizz`;
-CREATE TABLE IF NOT EXISTS `quizz` (
-  `id_quizz` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+CREATE TABLE `quizz` (
+  `id_quizz` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
   `date_quizz` date NOT NULL,
-  `type_quizz` int NOT NULL,
-  `id_user` int NOT NULL,
-  PRIMARY KEY (`id_quizz`),
-  KEY `user` (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Déchargement des données de la table `quizz`
---
-
-INSERT INTO `quizz` (`id_quizz`, `title`, `date_quizz`, `type_quizz`, `id_user`) VALUES
-(1, 'qu esst le soucis', '0000-00-00', 0, 2);
+  `type_quizz` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `role`
+-- Table structure for table `role`
 --
 
-DROP TABLE IF EXISTS `role`;
-CREATE TABLE IF NOT EXISTS `role` (
-  `id_role` tinyint NOT NULL AUTO_INCREMENT,
-  `title_role` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  PRIMARY KEY (`id_role`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+CREATE TABLE `role` (
+  `id_role` tinyint(4) NOT NULL,
+  `title_role` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `role`
+-- Dumping data for table `role`
 --
 
 INSERT INTO `role` (`id_role`, `title_role`) VALUES
-(1, 'Regular');
+(0, 'Utilisateur'),
+(1, 'Créateur de Quizz - Ecole'),
+(2, 'Créateur de Quizz - Entreprise');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `user`
+-- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id_user` int NOT NULL AUTO_INCREMENT,
-  `pseudo` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `mail` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `id_role` tinyint NOT NULL,
-  PRIMARY KEY (`id_user`),
-  KEY `Etrangere` (`id_role`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `pseudo` varchar(50) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `id_role` tinyint(4) NOT NULL DEFAULT '0',
+  `cle_api` varchar(60) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `user`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id_user`, `pseudo`, `mail`, `password`, `id_role`) VALUES
-(2, 'aaaaaa', 'aaaaa@gknsef.com', 'aaaaa', 1);
+INSERT INTO `user` (`id_user`, `pseudo`, `mail`, `password`, `id_role`, `cle_api`) VALUES
+(3, 'admin', 'williamhoangpro@gmail.com', '$2y$10$HTeo10zo/xvOpg0TgguHi.Nje8/7uSzerD6S93TJDNgoNczPiXEgu', 0, NULL),
+(4, 'zaki', 'zaki@zaki', '$2y$10$/uXq74bHkXZHkxDod2cGceEatsOvQIV7awMrtSfMRFmDTwx9QRjPm', 0, NULL),
+(5, 'alex', 'alex@alex', '$2y$10$IrLi5jSGLEVz/mortNl8i.w0dYmbm2oOMsNpeyhyoIedZ3xlt2oOy', 0, NULL);
 
 --
--- Contraintes pour les tables déchargées
+-- Indexes for dumped tables
 --
 
 --
--- Contraintes pour la table `feedback`
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id_feed`),
+  ADD KEY `question_f` (`id_question`),
+  ADD KEY `id_user_feed` (`id_user_feed`);
+
+--
+-- Indexes for table `note`
+--
+ALTER TABLE `note`
+  ADD PRIMARY KEY (`id_note`),
+  ADD KEY `note_user` (`id_user`),
+  ADD KEY `note_quizz` (`id_quizz`);
+
+--
+-- Indexes for table `qcm`
+--
+ALTER TABLE `qcm`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `question` (`id_question`);
+
+--
+-- Indexes for table `question`
+--
+ALTER TABLE `question`
+  ADD PRIMARY KEY (`id_question`),
+  ADD KEY `quizz` (`id_quizz`);
+
+--
+-- Indexes for table `quizz`
+--
+ALTER TABLE `quizz`
+  ADD PRIMARY KEY (`id_quizz`),
+  ADD KEY `user` (`id_user`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id_role`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `Etrangere` (`id_role`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id_feed` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `note`
+--
+ALTER TABLE `note`
+  MODIFY `id_note` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `qcm`
+--
+ALTER TABLE `qcm`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `question`
+--
+ALTER TABLE `question`
+  MODIFY `id_question` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `quizz`
+--
+ALTER TABLE `quizz`
+  MODIFY `id_quizz` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `id_role` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `feedback`
 --
 ALTER TABLE `feedback`
   ADD CONSTRAINT `id_user_feed` FOREIGN KEY (`id_user_feed`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `question_f` FOREIGN KEY (`id_question`) REFERENCES `question` (`id_question`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `note`
+-- Constraints for table `note`
 --
 ALTER TABLE `note`
   ADD CONSTRAINT `note_quizz` FOREIGN KEY (`id_quizz`) REFERENCES `quizz` (`id_quizz`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `note_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `qcm`
+-- Constraints for table `qcm`
 --
 ALTER TABLE `qcm`
   ADD CONSTRAINT `question` FOREIGN KEY (`id_question`) REFERENCES `question` (`id_question`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `question`
+-- Constraints for table `question`
 --
 ALTER TABLE `question`
   ADD CONSTRAINT `quizz` FOREIGN KEY (`id_quizz`) REFERENCES `quizz` (`id_quizz`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `quizz`
+-- Constraints for table `quizz`
 --
 ALTER TABLE `quizz`
   ADD CONSTRAINT `user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `user`
+-- Constraints for table `user`
 --
 ALTER TABLE `user`
   ADD CONSTRAINT `role_user` FOREIGN KEY (`id_role`) REFERENCES `role` (`id_role`) ON UPDATE CASCADE;
